50bad4a7cc632a8912a78d3078f074465a4b36f0
Disha <falcon.io0802@gmail.com>
2024-01-02 15:26:01 +0530

    Please enter the commit message for your changes. Lines starting no message

---
+++ calci.py
@@ -0,0 +1,20 @@
+#simple calculator where we take 2 input numbers and choice of operation
+#datatypes and variables
+#if else
+#operators
+
+
+x=input("enter 1st number: ")
+y=input("enter the 2nd number: ")
+print("choices: \nselect + for addition \n select - for subtraction ")
+choice=input("enter your choice: ")
+if choice=='+':
+    print(x+y)
+elif choice=='-':
+    print(x-y)
+
+
+#sum=x+y
+#print("{} + {} ={}".format(x,y,sum))
+
+
